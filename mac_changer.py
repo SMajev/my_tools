d99b869ab67c92a70df4e25c7234e8611ea799ba
SMajev <mekh@kali>
2021-10-16 23:29:49 +0200

    mac changer3

--- mac_changer.py
+++ mac_changer.py
@@ -1,13 +1,21 @@
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option('-i', '--interface', dest='interface',
+                 help='Interface to change its MAC address.')
+
+parser.add_option('-m', '--mac', dest='MAC',
+                 help='New MAC address example (00:11:22:33:44:66): ')
+
+
+parser.parse_args()
 
 interface = input('Interface: ')
 MAC = input('MAC address example(00:11:22:33:44:66): ')
 
 print(f'[*] Changing MAC address for {interface} to {MAC}')
-
-# subprocess.call(f'ifconfig {interface} down', shell=True)
-# subprocess.call(f'ifconfig {interface} hw ether {MAC}', shell=True)
-# subprocess.call(f'ifconfig {interface} up', shell=True)
 
 subprocess.call(['sudo', "ifconfig", interface, 'down'])
 subprocess.call(['sudo', "ifconfig", interface, 'hw', 'ether', MAC])
