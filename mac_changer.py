4b9125d60e3f8cd0b914094be69ea9ce9772861e
SMajev <mekh@kali>
2021-10-16 23:15:27 +0200

    mac changer3

--- mac_changer.py
+++ mac_changer.py
@@ -5,7 +5,10 @@ MAC = input('MAC address example(00:11:22:33:44:66): ')
 
 print(f'[*] Changing MAC address for {interface} to {MAC}')
 
-subprocess.call(f'ifconfig {interface} down', shell=True)
-subprocess.call(f'ifconfig {interface} hw ether {MAC}', shell=True)
-subprocess.call(f'ifconfig {interface} up', shell=True)
+# subprocess.call(f'ifconfig {interface} down', shell=True)
+# subprocess.call(f'ifconfig {interface} hw ether {MAC}', shell=True)
+# subprocess.call(f'ifconfig {interface} up', shell=True)
 
+subprocess.call(['sudo', "ifconfig", interface, 'down'])
+subprocess.call(['sudo', "ifconfig", interface, 'hw', 'ether', MAC])
+subprocess.call(['sudo', "ifconfig", interface, 'up'])
