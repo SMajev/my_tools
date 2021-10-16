2e38ba0307915ba6e8a65e5038634e81aade8cd5
SMajev <mekh@kali>
2021-10-16 22:46:05 +0200

    mac changer2

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,11 @@
 import subprocess
 
+interface = input('Interface: ')
+MAC = input('MAC address example(00:11:22:33:44:66): ')
 
-subprocess.call('ifconfig wlan0 down', shell=True)
-subprocess.call('ifconfig wlan0 hw ether 00:11:22:33:44:66', shell=True)
-subprocess.call('ifconfig wlan0 up', shell=True)
+print(f'[*] Changing MAC address for {interface} to {MAC}')
+
+subprocess.call(f'ifconfig {interface} down', shell=True)
+subprocess.call(f'ifconfig {interface} hw ether {MAC}', shell=True)
+subprocess.call(f'ifconfig {interface} up', shell=True)
 
