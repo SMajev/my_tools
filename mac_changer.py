b6aa6e9f6e74e8c38897d892ab8d86ba6f4ce88c
SMajev <mekh@kali>
2021-10-17 23:38:45 +0200

    mac changer6

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,19 @@
 import subprocess
 import optparse
 
+def get_args():
+    parser = optparse.OptionParser()
+
+    parser.add_option('-i', '--interface', dest='interface',
+                    help='Interface to change its MAC address.')
+
+    parser.add_option('-m', '--mac', dest='MAC',
+                    help='New MAC address example (00:11:22:33:44:66): ')
+
+    (options, arguments) = parser.parse_args()
+
+    return options.interface, options.MAC
+
 def change_mac(interface, new_mac):
     print(f'[*] Changing MAC address for {interface} to {MAC}')
     subprocess.call(['sudo', "ifconfig", interface, 'down'])
@@ -8,18 +21,7 @@ def change_mac(interface, new_mac):
     subprocess.call(['sudo', "ifconfig", interface, 'up'])
 
 
-parser = optparse.OptionParser()
-
-parser.add_option('-i', '--interface', dest='interface',
-                 help='Interface to change its MAC address.')
-
-parser.add_option('-m', '--mac', dest='MAC',
-                 help='New MAC address example (00:11:22:33:44:66): ')
-
-(options, arguments) = parser.parse_args()
-
-INTERFACE = options.interface
-MAC = options.MAC
+INTERFACE, MAC = get_args()
 
 if __name__ == '__main__': change_mac(INTERFACE, MAC)
 
