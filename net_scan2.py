e3f3e7b244bdfe968d999493dd653a06255536f3
SMajev <mekh@kali>
2021-10-18 16:32:49 +0200

    net scan 4

--- net_scan2.py
+++ net_scan2.py
@@ -1,11 +1,12 @@
 import scapy.all as scapy
 
-IP = '192.168.0.5/24'
+IP = '192.168.0.5'
 
 def scan(ip):
     arp_request = scapy.ARP()
+    arp_request.pdst = ip
     print(arp_request.summary())
-
+    # scapy.ls(scapy.ARP())
 
 if __name__ == '__main__':    
     scan(IP)
