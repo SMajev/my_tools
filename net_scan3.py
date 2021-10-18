8e1d2e6b924eb35f3dd2a810ad1ad6875b85cdf4
SMajev <mekh@kali>
2021-10-18 17:30:35 +0200

    net scan 1

--- net_scan3.py
+++ net_scan3.py
@@ -9,7 +9,7 @@ def scan(ip):
     answered_lst = scapy.srp(arp_request_broadcast, timeout=1)[0]
     
     for element in answered_lst:
-        print(element[1])
+        print(element[1].show())
         print('--------------------------------------------------------------------------------------------------')
 
 
