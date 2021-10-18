2c0b7a2bfe161ee88e8e4114beeda69718d0aff1
SMajev <mekh@kali>
2021-10-18 17:22:30 +0200

    net scan 1

--- net_scan3.py
+++ net_scan3.py
@@ -6,8 +6,8 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)[0]
-    print(answered.summary())
+    answered_lst = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    print(answered_lst.summary())
 
 
 
