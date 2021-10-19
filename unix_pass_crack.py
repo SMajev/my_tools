cf8cd63333bacd6dddc2559235901c41876ca23f
SMajev <mekh@kali>
2021-10-19 16:02:02 +0200

    final

--- unix_pass_crack.py
+++ unix_pass_crack.py
@@ -1,27 +1,23 @@
-import crypt
-
-def ciph_pass(passwd):
-    return crypt.crypt('egg', 'HX')
+from crypt import crypt
 
 def test_pass(crypt_pass):
-    salt = crypt_pass[0:2]
+    salt = crypt_pass[:2]
     dict_file = open('dictionary.txt', 'r')
     for word in dict_file.readlines():
-        word = word.strip('\n')
-        crypt_word = crypt.crypt(word, salt)
+        crypt_word = crypt(word.strip('\n'), salt)
         if crypt_word == crypt_pass:
             print(f'[+] Found Password: {word}\n')
             return
-        else:
-            print('[+] Password Not Found.\n')
-            return
+        
+    print('[-] Password Not Found.\n')
+    return
 
 def main():
     pass_file = open('password.txt')
     for line in pass_file.readlines():
         if ':' in line:
             user = line.split(':')[0]
-            crypt_pass = line.split(':')[1].strip(' ')
+            crypt_pass = line.split(':')[1].strip()
             print(f'[*] Cracking Password For: {user}')
             test_pass(crypt_pass)
 
